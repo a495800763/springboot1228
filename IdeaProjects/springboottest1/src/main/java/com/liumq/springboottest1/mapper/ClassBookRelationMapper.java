323c7c5e36f8d7d26af4a34d94f832d7133f4885
liumq-95 <495800763@qq.com>
2020-03-07 19:15:09 +0800

    添加一下杂七杂八的东西

--- IdeaProjects/springboottest1/src/main/java/com/liumq/springboottest1/mapper/ClassBookRelationMapper.java
+++ IdeaProjects/springboottest1/src/main/java/com/liumq/springboottest1/mapper/ClassBookRelationMapper.java
@@ -4,18 +4,10 @@ import com.liumq.springboottest1.entity.ClassBookRelation;
 import com.liumq.springboottest1.entity.ClassBookRelationKey;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface ClassBookRelationMapper {
 
-    int deleteByPrimaryKey(ClassBookRelationKey key);
-
-    int insert(ClassBookRelation record);
-
-    int insertSelective(ClassBookRelation record);
-
-    ClassBookRelation selectByPrimaryKey(ClassBookRelationKey key);
-
-    int updateByPrimaryKeySelective(ClassBookRelation record);
-
-    int updateByPrimaryKey(ClassBookRelation record);
+     List<ClassBookRelation> selectAll ();
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 柳梦琦课程管理系统  
-## 这是一个自己学习spring和springboot反的demo
+## 这是一个自己学习spring和springboot的demo
 * 希望自己能一直坚持写下去
 
 下面是一个可以点击的[连接](http://www.baidu.com)
